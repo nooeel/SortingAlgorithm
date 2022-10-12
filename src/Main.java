d2f09785ea10a6c72655678a6ff7fe15a1a79cb3
Noel Leon <unconfigured@null.spigotmc.org>
2022-10-12 07:45:51 +0200

    Output finished Timer finished Selection working

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,14 @@ public class Main {
 
         int anzahlInts = 100;
 
+
         Generator generator = new Generator(anzahlInts);
 
         int verfahren = getVerfahren();
 
+
+
+        generateOutput(generator, verfahren);
 
     }
 
@@ -29,7 +33,8 @@ public class Main {
         return scanner.nextInt();
     }
 
-    public ArrayList<Integer> zuSortieren(ArrayList<Integer> sortingList, int verfahren) {
+    public static ArrayList<Integer> sortierenOutput(ArrayList<Integer> sortingList, int verfahren) {
+
         if (verfahren == 0) {
             System.out.println("Du hast keine gültige Zahl ausgewählt");
 
@@ -54,6 +59,41 @@ public class Main {
         }
 
         return sortingList;
+    }
+
+    public static void generateOutput(Generator generator, int verfahren) {
+
+        System.out.print("Starte sortieren...   Verwendetes Verfahren ist: ");
+
+        if (verfahren == 1) {
+            System.out.print("Selection Sort.");
+        } else if (verfahren == 2) {
+            System.out.print("Insertion Sort.");
+        } else if (verfahren == 3) {
+            System.out.print("Bubble Sort.");
+        }
+
+        System.out.println("\n Der Timer wird jetzt gestartet...");
+
+        double timeBegin = System.nanoTime();
+
+        System.out.println("\n\nSortierte Liste:");
+        System.out.println("_______________________________________\n");
+
+
+        for (int i = 0; i < sortierenOutput(generator.getUnsortedList(), verfahren).size(); i++) {
+            System.out.print(generator.getUnsortedList().get(i) + ", ");
+            if (i%10 == 9) {
+                System.out.println();
+            }
+        }
+
+        double timeEnded = System.nanoTime();
+
+        System.out.println();
+        System.out.print("Das Sortieren hat ");
+        System.out.print(timeEnded - timeBegin);
+        System.out.print(" Sekunden gedauert.");
     }
 
 }
